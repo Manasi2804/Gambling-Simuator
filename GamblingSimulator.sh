ef82fbcbc7ddf45b9f80cba9cbbbb9666e0165af
Manasi2804 <shahmanasi281996@gmail.com>
2020-03-26 18:13:29 +0530

    Added: Display no of days won or lost with amount

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -1,25 +1,53 @@
+
 #!/bin/bash -x
 #Constants
 BET=1
 #Variables
-Stake=100
 win=0
 lose=0
 randomcheck=0
 counter=1
-
+day=1
+noofdays=20
+tempstake=100
+tempstake2=100
+wincounter=0
+lostcounter=0
 echo "Welcome to gambling simulation"
 
-randomcheck=$((RANDOM%2))
-case $randomcheck in
-	1) echo "You lost"
-			((lose++))
-			Stake=$((Stake-BET))
+for((day=1;day<=20;day++))
+do
+	Stake=100
+	halfstake=$(($Stake/2))
+	wincondition=$((Stake+halfstake))
+	losecondition=$((Stake-halfstake))
+	while(( $Stake!=$wincondition && $Stake!=$losecondition ))
+	do
+		randomcheck=$((RANDOM%2))
+		case $randomcheck in
+			1)
+				((lose++))
+				Stake=$((Stake-BET))
+				;;
+			2)
+				((win++))
+				Stake=$((Stake+BET))
+				;;
+		esac
+	done
+	case $Stake in
+		$wincondition)
+			echo "You won for the day $day"
+			echo "Total win amount: "$halfstake
 			;;
-	2) echo "You win"
-			((win++))
-			Stake=$((Stake+BET))
+		$losecondition)
+			echo "You lost for the day $day"
+			echo "Total lost amount:"$halfstake
 			;;
-esac
-echo" updated Stake:$Stake"
+	esac
+done
+echo "No of days  you lost:" $lostcounter
+echo "You lost by:" $((lostcounter*halfstake))
+echo "No of days you won:" $wincounter
+echo "You won by:" $((wincounter*halfstake))
 
