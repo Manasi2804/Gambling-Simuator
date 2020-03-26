8a181d29a0c5c3428ae04a58f1966dd46f492dba
Manasi2804 <shahmanasi281996@gmail.com>
2020-03-26 17:36:25 +0530

    Added: Resign condition for the day

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -7,9 +7,13 @@ win=0
 lose=0
 randomcheck=0
 counter=1
-
+halfstake=$(($Stake/2))
+wincondition=$((Stake+halfstake))
+losecondition=$((Stake-halfstake))
 echo "Welcome to gambling simulation"
 
+while(( $Stake!=$wincondition && $Stake!=$losecondition ))
+do
 randomcheck=$((RANDOM%2))
 case $randomcheck in
 	1) echo "You lost"
@@ -21,5 +25,7 @@ case $randomcheck in
 			Stake=$((Stake+BET))
 			;;
 esac
+done
+echo" you resigned for the day"
 echo" updated Stake:$Stake"
 
