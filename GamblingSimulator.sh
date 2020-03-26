11ea9e12f2c3c7b6a5138c18f7f0cc3f2da57243
Manasi2804 <shahmanasi281996@gmail.com>
2020-03-26 17:18:11 +0530

    Added:To gambler is win or loss

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -3,5 +3,23 @@
 BET=1
 #Variables
 Stake=100
+win=0
+lose=0
+randomcheck=0
+counter=1
 
 echo "Welcome to gambling simulation"
+
+randomcheck=$((RANDOM%2))
+case $randomcheck in
+	1) echo "You lost"
+			((lose++))
+			Stake=$((Stake-BET))
+			;;
+	2) echo "You win"
+			((win++))
+			Stake=$((Stake+BET))
+			;;
+esac
+echo" updated Stake:$Stake"
+
